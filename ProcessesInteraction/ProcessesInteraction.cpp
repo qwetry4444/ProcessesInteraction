d0f42489c7ad5883876dc8f0b212644c72adb893
NthTryMan <111993380+qwetry4444@users.noreply.github.com>
2024-03-13 16:09:46 +0500

    Update ProcessesInteraction.cpp

--- ProcessesInteraction/ProcessesInteraction.cpp
+++ ProcessesInteraction/ProcessesInteraction.cpp
@@ -1,14 +1,33 @@
-﻿#include <iostream>
-#include "namedpipeapi.h"
-#include "processthreadsapi.h"
+#include <iostream>
 #include <windows.h>
-#include <stdio.h>
-#include <conio.h>
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	HANDLE hRead, hWrite;
+
+	SECURITY_ATTRIBUTES secAttr;
+	secAttr.nLength = sizeof(secAttr);
+	secAttr.bInheritHandle = true;
+	secAttr.lpSecurityDescriptor = NULL;
+
+	if (!CreatePipe(&hRead, &hWrite, &secAttr, 0))
+	{
+		cout << "Error: The channel was not created";
+		return 0;
+	}
+
+	PROCESS_INFORMATION pi;
+	STARTUPINFO sa;
+	ZeroMemory(&sa, sizeof(sa));
+	ZeroMemory(&pi, sizeof(pi));
+	sa.cb = sizeof(STARTUPINFO);
+	sa.dwFlags = STARTF_USESTDHANDLES;
+	sa.hStdInput = hRead;
+	sa.hStdError = GetStdHandle(STD_OUTPUT_HANDLE);
+	sa.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+
 	int value1, value2;
 	cout << "Enter values:" << endl;
 	cout << "Value1 = ";
@@ -16,31 +35,17 @@ int main()
 	cout << "Value2 = ";
 	cin >> value2;
 
-	STARTUPINFO sa;
-	HANDLE hRead, hWrite;
-	SECURITY_ATTRIBUTES secAttr{ sizeof(sa), NULL, true };
-	CreatePipe(&hRead, &hWrite, &secAttr, 0);
+	WriteFile(hWrite, &value1, sizeof(value1), NULL, NULL);
+	WriteFile(hWrite, &value2, sizeof(value2), NULL, NULL);
 
-	if (!CreatePipe(&hRead, &hWrite, &secAttr, 0))
-	{
-		cout << "Error: The channel was not created";
-		return;
-	}
-
-	STARTUPINFO sa;
-	sa.cb = sizeof(STARTUPINFO);
-	sa.dwFlags = STARTF_USESTDHANDLES;
-	sa.hStdInput = hRead; 
-	sa.hStdError = GetStdHandle(STD_OUTPUT_HANDLE);
-	sa.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	if (!CreateProcess(NULL, "childProcess.exe", NULL, NULL, true, CREATE_NEW_CONSOLE, NULL, NULL, &sa, &sa))
+	if (!CreateProcess(L"ChildProcess.exe", NULL, NULL, NULL, true, CREATE_NEW_CONSOLE, NULL, NULL, &sa, &pi))
 	{
 		cout << "Error: The Child process was not created" << endl;
+
 		return 0;
 	}
 
-	WriteFile(write, buf, sizeof(buf), &bytes, NULL);
+
+
 
 };
-
